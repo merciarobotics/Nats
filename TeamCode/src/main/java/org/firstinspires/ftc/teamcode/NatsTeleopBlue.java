0c285dea7c39111033439f496bb64ba3a651aa7b
merciarobotics <merciarobotics@gmail.com>
2026-03-27 18:41:28 +0000

    Update com.pedropathing:ftc dependency to 2.1.1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NatsTeleopBlue.java
@@ -0,0 +1,276 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.pedropathing.util.Timer;
+
+@TeleOp
+public class NatsTeleopBlue {
+
+    double crawlMode;          // define variables here
+
+
+
+
+
+    private Follower follower;
+    private Timer pathTimer;
+
+
+
+
+
+    public enum PathState{
+        STANDBY,
+        FIELD_ORIENTATED_MANUAL_DRIVE_START,
+        FIELD_ORIENTATED_MANUAL_DRIVE,
+        ROBOT_ORIENTATED_MANUAL_DRIVE_START,
+        ROBOT_ORIENTATED_MANUAL_DRIVE,
+        RETURN_TO_START,
+        DRIVE_TO_BLUE_FINISH,
+        FAR_SHOOT_POS,
+        NEAR_SHOOT_POS,
+        GATE_POS,
+        NORMAL_MODE,
+        CRAWL_MODE
+
+    }
+
+    PathState drivetrainPathState;
+    PathState crawlModePathState;
+
+
+
+
+
+
+    private final Pose blueFinish = new Pose(39,33,Math.toRadians(90));
+    private final Pose closeBlueLaunchPose = new Pose(49,98,Math.toRadians(45));
+    private final Pose farBlueLaunchPose = new Pose(48.92957746478873,9.887323943661967,Math.toRadians(135));
+    private final Pose blueGatePos = new Pose(11.579661016949157,62.67118644067798,Math.toRadians(150));
+
+
+
+    private PathChain driveToRedFinish;
+    private PathChain driveToCloseLaunchPose;
+    private PathChain driveToFarLaunchPose;
+    private PathChain driveToBlueGatePos;
+
+
+
+    public void buildPaths(){
+
+        driveToRedFinish = follower.pathBuilder()
+                .addPath(new BezierLine(follower.getPose(),blueFinish))
+                .setLinearHeadingInterpolation(follower.getPose().getHeading(),blueFinish.getHeading())
+                .build();
+        driveToCloseLaunchPose = follower.pathBuilder()
+                .addPath(new BezierLine(follower.getPose(),closeBlueLaunchPose))
+                .setLinearHeadingInterpolation(follower.getPose().getHeading(),closeBlueLaunchPose.getHeading())
+                .build();
+        driveToFarLaunchPose = follower.pathBuilder()
+                .addPath(new BezierLine(follower.getPose(),farBlueLaunchPose))
+                .setLinearHeadingInterpolation(follower.getPose().getHeading(),farBlueLaunchPose.getHeading())
+                .build();
+        driveToBlueGatePos = follower.pathBuilder()
+                .addPath(new BezierLine(follower.getPose(),blueGatePos))
+                .setLinearHeadingInterpolation(follower.getPose().getHeading(),blueGatePos.getHeading())
+                .build();
+    }
+
+    public void statePathUpdate(){
+
+
+
+
+
+        switch(drivetrainPathState){
+
+            case FIELD_ORIENTATED_MANUAL_DRIVE_START:
+                follower.startTeleopDrive();
+                setDrivetrainPathState(PathState.FIELD_ORIENTATED_MANUAL_DRIVE);
+                break;
+
+            case FIELD_ORIENTATED_MANUAL_DRIVE:
+                follower.setTeleOpDrive(gamepad1.left_stick_y*crawlMode, gamepad1.left_stick_x*crawlMode, -gamepad1.right_stick_x*crawlMode,false);
+                break;
+
+            case ROBOT_ORIENTATED_MANUAL_DRIVE_START:
+                follower.startTeleopDrive();
+                setDrivetrainPathState(PathState.ROBOT_ORIENTATED_MANUAL_DRIVE);
+                break;
+
+            case ROBOT_ORIENTATED_MANUAL_DRIVE:
+                follower.setTeleOpDrive(-gamepad1.left_stick_y*crawlMode, -gamepad1.left_stick_x*crawlMode, -gamepad1.right_stick_x*crawlMode,true);
+
+                break;
+
+            case DRIVE_TO_BLUE_FINISH:
+                follower.followPath(driveToRedFinish,true);
+                //TODO change depending on end lift
+                setDrivetrainPathState(PathState.STANDBY);
+                break;
+            case FAR_SHOOT_POS:
+                follower.followPath(driveToFarLaunchPose,true);
+                setDrivetrainPathState(PathState.STANDBY);
+                break;
+            case NEAR_SHOOT_POS:
+                follower.followPath(driveToCloseLaunchPose,true);
+                setDrivetrainPathState(PathState.STANDBY);
+                break;
+            case GATE_POS:
+                follower.followPath(driveToBlueGatePos,true);
+                setDrivetrainPathState(PathState.STANDBY);
+                break;
+
+            case STANDBY:
+                break;
+
+            default:
+                drivetrainPathState = PathState.STANDBY;
+                break;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+        switch (crawlModePathState){
+            case NORMAL_MODE:
+                crawlMode = 1;
+                setCrawlModePathState(PathState.STANDBY);
+                break;
+
+            case CRAWL_MODE:
+                crawlMode = 0.25;
+                setCrawlModePathState(PathState.STANDBY);
+                break;
+
+            case STANDBY:
+                if(gamepad1.leftStickButtonWasPressed() && crawlMode == 0.25){
+                    setCrawlModePathState(PathState.NORMAL_MODE);
+                }if(gamepad1.leftStickButtonWasPressed() && crawlMode == 1){
+                setCrawlModePathState(PathState.CRAWL_MODE);
+            }
+
+                break;
+
+            default:
+                setCrawlModePathState(PathState.NORMAL_MODE);
+                break;
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+    public void setDriveMode() {
+        if (gamepad1.right_trigger > 0.9) {
+            setDrivetrainPathState(PathState.FIELD_ORIENTATED_MANUAL_DRIVE_START);
+        }
+        if (gamepad2.rightStickButtonWasPressed()) {
+            setDrivetrainPathState(PathState.ROBOT_ORIENTATED_MANUAL_DRIVE_START);               // custom methods here
+        }
+        if (gamepad1.rightBumperWasPressed()) {
+            setDrivetrainPathState(PathState.FAR_SHOOT_POS);
+        }
+        if (gamepad1.leftBumperWasPressed()) {
+            setDrivetrainPathState(PathState.NEAR_SHOOT_POS);
+        }
+        if (gamepad1.left_trigger > 0.9) {
+            setDrivetrainPathState(PathState.DRIVE_TO_BLUE_FINISH);
+        }
+        if (gamepad1.rightStickButtonWasPressed()) {
+            setDrivetrainPathState(PathState.GATE_POS);
+        }
+        if (gamepad1.rightBumperWasPressed()) {
+            setDrivetrainPathState(PathState.FAR_SHOOT_POS);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void setDrivetrainPathState(PathState newState){
+        drivetrainPathState = newState;                                        // set path states here
+        pathTimer.resetTimer();
+    }public void setCrawlModePathState(PathState newState){
+        drivetrainPathState = newState;
+    }
+
+
+
+
+
+
+
+
+
+
+    public void init(){
+        drivetrainPathState = PathState.FIELD_ORIENTATED_MANUAL_DRIVE_START;
+
+        pathTimer = new Timer();
+
+    }
+
+
+
+
+
+
+
+
+
+    public void start(){
+
+    }
+
+
+
+
+
+
+
+
+
+    public void loop(){
+        follower.update();
+        setDriveMode();
+
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,19 +1,55 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.OTOSConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(17)
+            .headingPIDFCoefficients(new PIDFCoefficients(1.3,0,0.02,0.05))
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.12,0,0.0096,0.05))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0,0.00001,0.6,0.001))
+            .forwardZeroPowerAcceleration(-59)
+            .lateralZeroPowerAcceleration(-70);
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightBack")
+            .leftRearMotorName("leftBack")
+            .leftFrontMotorName("leftFront")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(46.3)
+            .yVelocity(32);
+    public static OTOSConstants localizerConstants = new OTOSConstants()
+            .hardwareMapName("otos")
+            .linearUnit(DistanceUnit.INCH)
+            .angleUnit(AngleUnit.RADIANS)
+            .offset(new SparkFunOTOS.Pose2D(-6.25, 2.46, -Math.PI/2))
+            .linearScalar(0.980914)
+            .angularScalar(0.991723689);
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 0.9, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .OTOSLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
                 .build();
     }
 }
